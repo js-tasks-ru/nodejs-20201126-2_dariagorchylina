a278d89bc5cee8f621eb13f492d45f42a582b945
daria.gorchylina <daria.gorchylina@gmail.com>
2020-12-07 00:07:02 +0200

    03-streams: 01-limit-size-stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,22 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
+  #limit = 0;
+  #sumOfChunks= 0;
   constructor(options) {
     super(options);
+    this.#limit = options.limit
   }
 
   _transform(chunk, encoding, callback) {
-
+    let error;
+    this.#sumOfChunks += chunk.length;
+    if (this.#sumOfChunks > this.#limit) {
+      error = new LimitExceededError()
+      callback(error);
+    } else {
+      callback(error, chunk);
+    }
   }
 }
 
