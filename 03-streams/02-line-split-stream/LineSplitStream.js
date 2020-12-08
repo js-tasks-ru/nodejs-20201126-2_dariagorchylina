8638aa2908e36addcb6b0153db067d68e955c5e1
daria.gorchylina <daria.gorchylina@gmail.com>
2020-12-08 23:21:40 +0200

    03-streams 02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -2,14 +2,21 @@ const stream = require('stream');
 const os = require('os');
 
 class LineSplitStream extends stream.Transform {
+  #currentEl='';
   constructor(options) {
     super(options);
   }
 
   _transform(chunk, encoding, callback) {
+    const lines = (this.#currentEl + chunk.toString()).split(os.EOL);
+    this.#currentEl = lines.pop();
+    for (let line of lines) { this.push(line); }
+    callback()
   }
 
   _flush(callback) {
+    this.push(this.#currentEl != null ? this.#currentEl :"");
+    callback();
   }
 }
 
