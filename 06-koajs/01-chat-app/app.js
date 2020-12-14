c61cc7823f4b10389549ecd60d40ff9e6bd80186
daria.gorchylina <daria.gorchylina@gmail.com>
2020-12-14 22:14:45 +0200

    06-koajs 01-chat-app

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const Koa = require('koa');
+const {EventEmitter} = require('events');
+
+const ee = new EventEmitter();
 const app = new Koa();
 
 app.use(require('koa-static')(path.join(__dirname, 'public')));
@@ -9,9 +12,25 @@ const Router = require('koa-router');
 const router = new Router();
 
 router.get('/subscribe', async (ctx, next) => {
+  const message = await new Promise((resolve, reject) => {
+    ee.on('newMessage', (message) => {
+      resolve(message);
+      ee.removeAllListeners('newMessage');
+    });
+  });
+
+  ctx.status = 200;
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const {message} = ctx.request.body;
+  if (!message) {
+    return;
+  }
+  ee.emit('newMessage', message);
+  ctx.status = 201;
+  ctx.body = {message};
 });
 
 app.use(router.routes());
